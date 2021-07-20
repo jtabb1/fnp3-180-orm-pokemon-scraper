0b43baaa4d4ecab8d64902db11a303b3648af651
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-20 15:56:59 -0400

    Student's solution utilized peek of 1 of 2 files of the official solution to pass all tests.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
 class Pokemon
+
+  attr_accessor :name, :type
+  attr_reader :id, :db
+
+  def initialize(name:, type:, id: nil, db:)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+  end
+
+  def self.save(name,type,db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) 
+      VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id,db)
+    sql = <<-SQL
+      SELECT * FROM pokemon 
+      WHERE id = ?
+    SQL
+    rw = db.execute(sql, id)[0]
+    self.new(name: rw[1], type: rw[2], id: rw[0], db: db)
+  end
+
+
+
 end
